6386122b8a893a1f45ab9128854b1c4e080397f8
Giuseppe Lo Re <loregiuseppe0@gmail.com>
2022-11-09 16:11:07 +0100

    Milestone 1 completata

---
+++ Evento.cs
@@ -0,0 +1,123 @@
+﻿public partial class GestoreEventi
+{
+    public class Evento
+    {
+        private string _titolo;
+        private DateTime _data;
+        private int _massimaCapienzaEvento;
+        private int _numeroPostiPrenotati;
+
+        public string Titolo
+        {
+            get
+            {
+                return _titolo;
+            }
+            set
+            {
+                if(value == "")
+                {
+                    throw new Exception("Il titolo non può essere vuoto");
+                }
+                else
+                {
+                    _titolo = value;
+                }
+            }
+        }
+
+        public DateTime Data
+        {
+            get { 
+                return _data;
+            }
+            set
+            {
+                if(value < DateTime.Now)
+                {
+                    throw new Exception("La data non può essere precedente a quella odierna");
+                }
+                else
+                {
+                    Data = value;
+                }
+            }
+        }
+        public int MassimaCapienzaEvento
+        {
+            get
+            {
+                return _massimaCapienzaEvento;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new Exception("La capacità massima dell'evento non può essere minore o uguale a 0");
+                }
+                else
+                {
+                    _massimaCapienzaEvento = value;
+                }
+            }
+        }
+        public int NumeroPostiPrenotati
+        {
+            get
+            {
+                return _numeroPostiPrenotati;
+            }
+            private set
+            {
+                _numeroPostiPrenotati = value;
+            }
+        }
+        public Evento(string titolo, DateTime data, int massimaCapienzaEvento)
+        {
+            Titolo = titolo;
+            Data = data;
+            MassimaCapienzaEvento = massimaCapienzaEvento;
+            NumeroPostiPrenotati = 0;
+        }
+        private int NumeroPostiDisponibili()
+        {
+            return MassimaCapienzaEvento - NumeroPostiPrenotati;
+        }
+
+        public void PrenotaPosti(int posti)
+        {
+            if(Data < DateTime.Now)
+            {
+                throw new Exception("L'evento è terminato");
+            }
+            if(posti > NumeroPostiDisponibili())
+            {
+                throw new Exception("Numero posti disponibili insufficiente");
+            }
+            else
+            {
+                NumeroPostiPrenotati -= posti;
+            }
+        }
+        public void DisdiciPosti(int posti)
+        {
+            if(posti < 1)
+            {
+                throw new Exception("Il numero dei posti da sdidire deve essere minimo 1");
+            }
+            if(posti > NumeroPostiPrenotati)
+            {
+                throw new Exception("Il numero dei posti da sdidire non può essere superiore al numero posti prenotati");
+            }
+            if(Data < DateTime.Now)
+            {
+                throw new Exception("L'evento è terminato");
+            }
+        }
+        public override string ToString()
+        {
+            return Data.ToString("dd/MM/yyyy") + "-" + Titolo; 
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics.Tracing;
+using System.Runtime.ConstrainedExecution;
+using static GestoreEventi;
+
+
+
+
+
